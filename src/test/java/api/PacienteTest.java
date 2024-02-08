123b250346d9bb5cb1e85451835c2358ec52db1b
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-02-08 17:32:13 -0300

    Finished test on api/TestPaciente

--- src/test/java/api/PacienteTest.java
+++ src/test/java/api/PacienteTest.java
@@ -4,6 +4,7 @@ import config.ConfigProperties;
 import config.UserCredentialsConfig;
 import controllers.PacienteControllers;
 import exceptions.MissingPropertyException;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import models.Domicilio;
 import models.Paciente;
@@ -33,12 +34,26 @@ public class PacienteTest {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     String fechaIngresoString = localDate.format(formatter);
 
+    LocalDate yesterday = LocalDate.now().minusDays(1);
+
+    String fechaIngresoPasadaString = yesterday.format(formatter);
+
     Domicilio domicilio1 = new Domicilio("Calle Test", 2024, "Montevideo");
 
     Domicilio domicilio2 = new Domicilio("Calle Test 2", 2025, "Montevideo");
 
     Paciente pacienteNew1 = new Paciente("Paciente1", "Test1", "12345678", fechaIngresoString, domicilio1);
     Paciente pacienteNew2 = new Paciente("Paciente2", "Test2", "123456789", fechaIngresoString, domicilio2);
+
+    Paciente pacienteWithoutName = new Paciente("", "Test3", "123456789", fechaIngresoString, domicilio2);
+
+    Paciente pacienteWithoutLastName = new Paciente("PacienteCuatro", "", "123456789", fechaIngresoString, domicilio2);
+
+    Paciente pacienteWithoutDNI= new Paciente("PacienteCuatro", "Test4", "", fechaIngresoString, domicilio2);
+
+    Paciente pacienteWithDNILarge11= new Paciente("PacienteCuatro", "Test4", "12345678911", fechaIngresoString, domicilio2);
+
+    Paciente pacienteWithLastFechaIngreso = new Paciente("PacienteCuatro", "Test4", "123456789", fechaIngresoPasadaString, domicilio2);
 
     public PacienteTest() throws MissingPropertyException, IOException {
     }
@@ -184,5 +199,126 @@ public class PacienteTest {
     }
 
 
+    // TESTS WITH USER
+    @Test
+    @Order(10)
+    public void shouldNotPostToPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
 
+        Response res = pacienteControllers.postPaciente(pacienteNew2, base64CredentialsUser);
+
+        Assert.assertEquals(res.getStatusCode(), 403);
+    }
+
+    @Test
+    @Order(11)
+    public void shouldNotGetPacientes() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.getPacientes(base64CredentialsUser);
+
+        Assert.assertEquals(res.getStatusCode(), 403);
+    }
+
+    @Test
+    @Order(12)
+    public void shouldNotGetPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.getPaciente(2L, base64CredentialsUser);
+
+        Assert.assertEquals(res.getStatusCode(), 403);
+    }
+
+    @Test
+    @Order(13)
+    public void shouldNotDeletePacientes() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.deletePaciente(2L, base64CredentialsUser);
+
+        Assert.assertEquals(res.getStatusCode(), 403);
+    }
+
+
+    // TEST WITHOUT DATA ON BODY
+    @Test
+    @Order(14)
+    public void shouldMessageNameEmptyToPostPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.postPaciente(pacienteWithoutName, base64CredentialsAdmin);
+
+        String resBody = res.getBody().asString();
+
+        JsonPath jsonPathRes = new JsonPath(resBody);
+
+        String messageError = jsonPathRes.getString("nombre");
+
+        Assert.assertEquals(messageError, "Debe especificarse el nombre del paciente, no puede quedar vacio");
+    }
+
+    @Test
+    @Order(15)
+    public void shouldMessageLastNameEmptyToPostPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.postPaciente(pacienteWithoutLastName, base64CredentialsAdmin);
+
+        String resBody = res.getBody().asString();
+
+        JsonPath jsonPathRes = new JsonPath(resBody);
+
+        String messageError = jsonPathRes.getString("apellido");
+
+        Assert.assertEquals(messageError, "Debe especificarse el apellido del paciente, no puede quedar vacio");
+    }
+
+    @Test
+    @Order(16)
+    public void shouldMessageDNIEmptyToPostPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.postPaciente(pacienteWithoutDNI, base64CredentialsAdmin);
+
+        String resBody = res.getBody().asString();
+
+        JsonPath jsonPathRes = new JsonPath(resBody);
+
+        String messageError = jsonPathRes.getString("dni");
+
+        Assert.assertEquals(messageError, "El dni debe tener de largo entre 5 a 10 caracteres");
+    }
+
+    @Test
+    @Order(16)
+    public void shouldMessageDNILarge11ToPostPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.postPaciente(pacienteWithDNILarge11, base64CredentialsAdmin);
+
+        String resBody = res.getBody().asString();
+
+        JsonPath jsonPathRes = new JsonPath(resBody);
+
+        String messageError = jsonPathRes.getString("dni");
+
+        Assert.assertEquals(messageError, "El dni debe tener de largo entre 5 a 10 caracteres");
+    }
+
+    @Test
+    @Order(17)
+    public void shouldMessageFechaIngresoPasadaToPostPaciente() throws MissingPropertyException, IOException {
+        PacienteControllers pacienteControllers = new PacienteControllers();
+
+        Response res = pacienteControllers.postPaciente(pacienteWithLastFechaIngreso, base64CredentialsAdmin);
+
+        String resBody = res.getBody().asString();
+
+        JsonPath jsonPathRes = new JsonPath(resBody);
+
+        String messageError = jsonPathRes.getString("fechaIngreso");
+
+        Assert.assertEquals(messageError, "La fecha no puede ser anterior al día de hoy");
+    }
 }
