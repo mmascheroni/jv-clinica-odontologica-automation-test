1a091410b577267d23d393856b1b6e94aa373184
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-02-18 15:41:19 -0300

    Advanced on UI OdontologoTest

--- src/test/java/ui/OdontologoTest.java
+++ src/test/java/ui/OdontologoTest.java
@@ -3,18 +3,20 @@ package ui;
 import config.ConfigProperties;
 import exceptions.MissingPropertyException;
 import models.Odontologo;
-import org.junit.jupiter.api.*;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import pages.OdontologoPage;
 
 import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OdontologoTest {
 
     public WebDriver driver;
@@ -23,13 +25,15 @@ public class OdontologoTest {
 
     private Odontologo odontologo1 = new Odontologo("OdontologoUno", "testUno", "ODO001");
 
+    private Odontologo odontologo2 = new Odontologo("OdontologoTres", "testTres", "ODO003");
+
     private String getConfigProperty(String property, String propFileName) throws IOException, MissingPropertyException {
         configProperties.loadProperties(propFileName);
 
         return configProperties.getProp(property);
     }
 
-    @BeforeAll
+    @BeforeClass
     public void preconditions() throws MissingPropertyException, IOException {
         driver = new ChromeDriver();
 
@@ -41,8 +45,8 @@ public class OdontologoTest {
         odontologoPage.navigateToOdontologo(getConfigProperty("USER_ADMIN", "config"), getConfigProperty("PASSWORD_ADMIN", "config"));
     }
 
-    @Test
-    public void AddOdontologo() {
+    @Test(priority = 1)
+    public void addOdontologoWithId1() {
         OdontologoPage odontologoPage = new OdontologoPage(driver, null);
 
         odontologoPage.navigateToFormToAddOdontologo();
@@ -66,8 +70,65 @@ public class OdontologoTest {
 
     }
 
+    @Test(priority = 2)
+    public void deleteFirstOdontologoInTable() {
+        OdontologoPage odontologoPage = new OdontologoPage(driver, null);
+        odontologoPage.clickBtnDeleteFirstOdontologoInTable();
+        odontologoPage.acceptAlertToDeleteOdontologo();
 
-    @AfterAll
+        String emptyTable = odontologoPage.odontologoTableIsEmpty();
+
+        Assert.assertEquals(emptyTable, "No hay registros actualmente...");
+
+    }
+
+    @Test(priority = 3)
+    public void addOdontologoWithId2() {
+        OdontologoPage odontologoPage = new OdontologoPage(driver, null);
+
+        odontologoPage.navigateToFormToAddOdontologo();
+        odontologoPage.insertName(odontologo2.getNombre());
+        odontologoPage.insertLastName(odontologo2.getApellido());
+        odontologoPage.insertMatricula(odontologo2.getMatricula());
+        odontologoPage.clickBtnFormAddOdontologo();
+
+        String odontologoId = odontologoPage.getIdInTableFirstRow();
+        String odontologoName = odontologoPage.getNameInTableFirstRow();
+        String odontologoLastName = odontologoPage.getLastNameInTableFirstRow();
+        String odontologoMatricula = odontologoPage.getMatriculaInTableFirstRow();
+
+        Assert.assertEquals(parseInt(odontologoId), 2);
+        Assert.assertEquals(odontologoName, odontologo2.getNombre());
+        Assert.assertEquals(odontologoLastName, odontologo2.getApellido());
+        Assert.assertEquals(odontologoMatricula, odontologo2.getMatricula());
+    }
+
+    @Test(priority = 4)
+    public void modifyingOdontologoWithId2() {
+        OdontologoPage odontologoPage = new OdontologoPage(driver, null);
+
+        Odontologo odontologo2ToModify = new Odontologo("OdontologoDos", "testDos", "ODO002");
+
+        odontologoPage.navigateToModifyOdontologoForm();
+        odontologoPage.insertNameToModify(odontologo2ToModify.getNombre());
+        odontologoPage.insertLastNameToModify(odontologo2ToModify.getApellido());
+        odontologoPage.insertMatriculaToModify(odontologo2ToModify.getMatricula());
+        odontologoPage.clickBtnConfirmModifyOdontologo();
+
+
+        String odontologoId = odontologoPage.getIdInTableFirstRow();
+        String odontologoName = odontologoPage.getNameInTableFirstRow();
+        String odontologoLastName = odontologoPage.getLastNameInTableFirstRow();
+        String odontologoMatricula = odontologoPage.getMatriculaInTableFirstRow();
+
+        Assert.assertEquals(parseInt(odontologoId), 2);
+        Assert.assertEquals(odontologoName, odontologo2ToModify.getNombre());
+        Assert.assertEquals(odontologoLastName, odontologo2ToModify.getApellido());
+        Assert.assertEquals(odontologoMatricula, odontologo2ToModify.getMatricula());
+    }
+
+
+    @AfterClass
     public void tearDown() {
         OdontologoPage odontologoPage = new OdontologoPage(driver, null);
 
