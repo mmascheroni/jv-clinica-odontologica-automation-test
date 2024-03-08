5a9cf9b33bba91a75085660ec56fb428f455fbe2
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-03-08 17:30:36 -0300

    Added more tests cases to PacienteTest

--- src/test/java/ui/PacienteTest.java
+++ src/test/java/ui/PacienteTest.java
@@ -32,14 +32,19 @@ public class PacienteTest {
 
     Domicilio domicilio = new Domicilio("calle Test", 1234, "localidad Test");
 
+    Domicilio domicilioToModify = new Domicilio("calle Modify Test", 1111, "localidad Modify Test");
+
     private LocalDateAdapter localDateAdapter = new LocalDateAdapter();
 
     String fechaIngresoString = localDateAdapter.localDateToString(LocalDate.now(), "dd-MM-YYYY");
 
     String fechaIngresoStringToAsserts = localDateAdapter.localDateToString(LocalDate.now(), "YYYY-MM-dd");
 
-    Paciente pacienteNew1 = new Paciente("PacienteUno", "PacienteDos", "1234567", fechaIngresoString, domicilio);
+    Paciente pacienteNew1 = new Paciente("PacienteUno", "TestUno", "1234567", fechaIngresoString, domicilio);
 
+    Paciente pacienteToModify1 = new Paciente("PacienteModifyUno", "TestModifyUno", "1234666", fechaIngresoString, domicilioToModify);
+
+    Paciente pacienteNew2 = new Paciente("PacienteDos", "TestDos", "1234568", fechaIngresoString, domicilio);
 
     @BeforeClass
     public void preconditions() throws MissingPropertyException, IOException {
@@ -92,8 +97,78 @@ public class PacienteTest {
         Assert.assertEquals(pacienteDNI, pacienteNew1.getDni());
         Assert.assertEquals(pacienteIncomeDate, fechaIngresoStringToAsserts);
         Assert.assertEquals(pacienteStreet, pacienteNew1.getDomicilio().getCalle());
-        Assert.assertEquals(pacienteNumber, pacienteNew1.getDomicilio().getNumero());
+        Assert.assertEquals(parseInt(pacienteNumber), pacienteNew1.getDomicilio().getNumero());
         Assert.assertEquals(pacienteLocation, pacienteNew1.getDomicilio().getLocalidad());
+    }
+
+    @Test(priority = 3)
+    public void modifyPacienteWithId1() {
+        PacientePage pacientePage = new PacientePage(driver, null);
+
+        pacientePage.navigateToFormToModifiyPaciente();
+        pacientePage.modifyInsertName(pacienteToModify1.getNombre());
+        pacientePage.modifyInsertLastName(pacienteToModify1.getApellido());
+        pacientePage.modifyInsertDNI(pacienteToModify1.getDni());
+        pacientePage.modifyInsertIncomeDate(pacienteToModify1.getFechaIngreso());
+        pacientePage.modifyInsertStreet(pacienteToModify1.getDomicilio().getCalle());
+        pacientePage.modifyInsertNumber(pacienteToModify1.getDomicilio().getNumero());
+        pacientePage.modifyInsertLocation(pacienteToModify1.getDomicilio().getLocalidad());
+        pacientePage.modifiyPaciente();
+
+        String pacienteId = pacientePage.getFirstIdOnTable();
+        String pacienteName = pacientePage.getPacienteAddedIntoTable(1, 2);
+        String pacienteLastName = pacientePage.getPacienteAddedIntoTable(1, 3);
+        String pacienteDNI = pacientePage.getPacienteAddedIntoTable(1, 4);
+        String pacienteIncomeDate = pacientePage.getPacienteAddedIntoTable(1, 5);
+        String pacienteStreet = pacientePage.getPacienteAddedIntoTable(1, 7);
+        String pacienteNumber = pacientePage.getPacienteAddedIntoTable(1, 8);
+        String pacienteLocation = pacientePage.getPacienteAddedIntoTable(1, 9);
+
+        Assert.assertEquals(pacientePage.tableRows().size(), 1);
+        Assert.assertEquals(parseInt(pacienteId), 1);
+        Assert.assertEquals(pacienteName, pacienteToModify1.getNombre());
+        Assert.assertEquals(pacienteLastName, pacienteToModify1.getApellido());
+        Assert.assertEquals(pacienteDNI, pacienteToModify1.getDni());
+        Assert.assertEquals(pacienteIncomeDate, fechaIngresoStringToAsserts);
+        Assert.assertEquals(pacienteStreet, pacienteToModify1.getDomicilio().getCalle());
+        Assert.assertEquals(parseInt(pacienteNumber), pacienteToModify1.getDomicilio().getNumero());
+        Assert.assertEquals(pacienteLocation, pacienteToModify1.getDomicilio().getLocalidad());
+    }
+
+
+
+    @Test(priority = 4)
+    public void addPacienteWithId2() {
+        PacientePage pacientePage = new PacientePage(driver, null);
+
+        pacientePage.navigateToFormToAddPaciente();
+        pacientePage.insertName(pacienteNew2.getNombre());
+        pacientePage.insertLastName(pacienteNew2.getApellido());
+        pacientePage.insertDNI(pacienteNew2.getDni());
+        pacientePage.insertIncomeDate(pacienteNew2.getFechaIngreso());
+        pacientePage.insertStreet(pacienteNew2.getDomicilio().getCalle());
+        pacientePage.insertNumber(pacienteNew2.getDomicilio().getNumero());
+        pacientePage.insertLocation(pacienteNew2.getDomicilio().getLocalidad());
+        pacientePage.addPaciente();
+
+        String pacienteId = pacientePage.getPacienteAddedIntoTable(2, 1);
+        String pacienteName = pacientePage.getPacienteAddedIntoTable(2, 2);
+        String pacienteLastName = pacientePage.getPacienteAddedIntoTable(2, 3);
+        String pacienteDNI = pacientePage.getPacienteAddedIntoTable(2, 4);
+        String pacienteIncomeDate = pacientePage.getPacienteAddedIntoTable(2, 5);
+        String pacienteStreet = pacientePage.getPacienteAddedIntoTable(2, 7);
+        String pacienteNumber = pacientePage.getPacienteAddedIntoTable(2, 8);
+        String pacienteLocation = pacientePage.getPacienteAddedIntoTable(2, 9);
+
+        Assert.assertEquals(pacientePage.tableRows().size(), 2);
+        Assert.assertEquals(parseInt(pacienteId), 2);
+        Assert.assertEquals(pacienteName, pacienteNew2.getNombre());
+        Assert.assertEquals(pacienteLastName, pacienteNew2.getApellido());
+        Assert.assertEquals(pacienteDNI, pacienteNew2.getDni());
+        Assert.assertEquals(pacienteIncomeDate, fechaIngresoStringToAsserts);
+        Assert.assertEquals(pacienteStreet, pacienteNew2.getDomicilio().getCalle());
+        Assert.assertEquals(parseInt(pacienteNumber), pacienteNew2.getDomicilio().getNumero());
+        Assert.assertEquals(pacienteLocation, pacienteNew2.getDomicilio().getLocalidad());
     }
 
 
