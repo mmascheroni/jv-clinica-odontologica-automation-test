321e7bc6cb38359f15b0d45d1099d79eb1fd10fb
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-03-11 19:34:49 -0300

    Created TurnoPage and TurnoTest, are empties

---
+++ src/main/java/pages/TurnoPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TurnoPage extends BasePage{
+
+    public TurnoPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+}
